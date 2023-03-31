c79d4ce75d01477dc98e83b6db50a56db69150cf
KylePM0 <kylepm06@gmail.com>
2023-03-31 13:35:49 -0400

    Did the massive clean-up. May the powers that be help us all

--- mission_hill.v1/mission_hill/startbootstrap-clean-blog-gh-pages/createdb.js
+++
@@ -1,5 +0,0 @@
-const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('messages.db');
-
-const sql = 'CREATE TABLE IF NOT EXISTS messages (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, email TEXT, phone TEXT, message TEXT)';
-db.run(sql);
